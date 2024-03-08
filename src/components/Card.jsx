a16005b69bf506d1ab0931d72c4014e9ccd3001e
Kar <99517165+Karit08@users.noreply.github.com>
2024-03-07 20:28:57 -0600

    Se agregaron estilos del componente Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from '../styles/Card.module.css'
 
 export default function Card(props) { //export default function Card({onClose, name, min, max, img}) {
   // acá va tu código //Card Component
 
   // let min =((props.min - 32) /1.8) aqui lo trato de convertir a Farenheit <p>{min}</p>
+  // {props.name === "Cairns" ? styles.card1 : styles.card } se puede hacer condicionales en estilos
 
   return (
-  <div>
-    <button onClick={props.onClose}>X</button>
+  <div className={styles.card}>
+    <button className={`${styles.btn} ${styles.btnColor}`} onClick={props.onClose}>X</button>
     <h4>{props.name} </h4>
     <p>Mín</p>
     <p>{props.min}</p> 
